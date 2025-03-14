4b37fe321b2de6b016d3727201e45e07a51b7a71
natha <natha@LAPTOP-A2NGKOK6>
2025-03-14 09:23:12 +0100

    Add project files.

---
+++ CSSmall/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YourNamespace.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string email, string password)
+        {
+            if (username == "test" && password == "1234")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Fel användarnamn eller lösenord.";
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ CSSmall/Controllers/RoomsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSSmall.Controllers
+{
+    public class RoomsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DetailsStandard()
+        {
+            return View();
+        }
+
+        public IActionResult DetailsDeluxe()
+        {
+            return View();
+        }
+
+        public IActionResult DetailsSvit() 
+        {
+            return View();
+        }
+    }
+}
